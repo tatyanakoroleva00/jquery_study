d467fa884e7b992c10bb3ee8be2c2a4a2055f9f6
Tatiana <tatyanakoroleva00@mail.ru>
2024-07-07 20:05:25 +0300

    lesson 8

--- 8/task.js
+++ 8/task.js
@@ -1,35 +1,35 @@
-// $(document).ready(function(){
-//         $.ajax({
-//             url: "https://jsonplaceholder.typicode.com/todos/1", 
-//             success: function(result){
-//                 console.log(result.userId);
-//                 $('p').text(result.userId);
-//             }
-//         })
+$(document).ready(function(){
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/todos/1", 
+            success: function(result){
+                console.log(result.userId);
+                $('p').text(result.userId);
+            }
+        })
 
-// })
+})
 
-// Создайте запрос на получение 100 
-// постов и отправьте запрос на удаление 10-го поста из списка
+Создайте запрос на получение 100 
+постов и отправьте запрос на удаление 10-го поста из списка
 
-// $(document).ready(function(){
-//     $('button').click(()=> {
-//         $.get("https://jsonplaceholder.typicode.com/users/1/posts", 
-//             function(data){
-//                 console.log(data.slice(0, 5));
-//             }
-//         )
-//     })
+$(document).ready(function(){
+    $('button').click(()=> {
+        $.get("https://jsonplaceholder.typicode.com/users/1/posts", 
+            function(data){
+                console.log(data.slice(0, 5));
+            }
+        )
+    })
 
-// })
-// $(document).ready(()=> {
-//     $('.delete').click(()=> {
-//         $.ajax({
-//             url: "https://jsonplaceholder.typicode.com/users/1/posts", 
-//             type: 'DELETE',
-//             success: function(data){
-//                 console.log(data);
-//             }
-//         })
-//     })
-// })
+})
+$(document).ready(()=> {
+    $('.delete').click(()=> {
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/users/1/posts", 
+            type: 'DELETE',
+            success: function(data){
+                console.log(data);
+            }
+        })
+    })
+})
